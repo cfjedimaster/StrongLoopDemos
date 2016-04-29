833ebf63c5abfe88cb05708c83437e23c483bd25
Raymond Camden <raymondcamden@gmail.com>
2016-04-29 16:32:12 -0500

    filter max

--- superlongscroll/common/models/person.js
+++ superlongscroll/common/models/person.js
@@ -1,3 +1,11 @@
 module.exports = function(Person) {
 
+	Person.beforeRemote('find', function(ctx, instance, next) {
+		if(!ctx.args.filter || !ctx.args.filter.limit) {
+			console.log('forcing limit!');
+			if(!ctx.args.filter) ctx.args.filter = {};
+			ctx.args.filter.limit = 10;
+		}
+		next();
+	});
 };
